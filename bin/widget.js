fdea14aea2cb2b23c2e495f06b03f7bdeab44f86
genify <genify@163.com>
2014-07-22 14:20:46 +0800

    support for nej-widget for util template

---
+++ bin/widget.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var _fs = require('../lib/file.js');
+    
+// init args
+var _arr = process.argv.slice(2),
+    _dst = (_arr[0]||'').trim()||process.cwd();
+if (!/\/$/.test(_dst)){
+    _dst += '/';
+}
+
+// make dir
+_fs.copy(
+    __dirname+'/../template/widget/',
+    _dst,function(_from,_to){
+        console.log('Create %s',_to);
+    }
+);
+console.log('Widget Create Success!');
---
+++ template/widget/widget.js
@@ -0,0 +1,57 @@
+/*
+ * ------------------------------------------
+ * 
+ * @version  1.0
+ * @author   
+ * ------------------------------------------
+ */
+define(['{lib}util/event.js'],
+function(){
+    var _  = NEJ.P,
+        _t = _('nej.ut'),
+        _p = _('nm.ut'),
+        _pro;
+    /**
+     * 
+     * @class   {nm.ut._$$Class}
+     * @extends {nej.ut._$$Event}
+     * 
+     * @param   {Object} 可选配置参数，已处理参数列表如下
+     *
+     */
+    _p._$$Class = NEJ.C();
+    _pro = _p._$$Class._$extend(_t._$$Event);
+    /**
+     * 控件初始化
+     * @protected
+     * @method {__init}
+     * @return {Void}
+     */
+    _pro.__init = function(){
+        this.__supInit();
+        // TODO
+    };
+    /**
+     * 控件重置
+     * @protected
+     * @method {__reset}
+     * @param  {Object} 可选配置参数
+     * @return {Void}
+     */
+    _pro.__reset = function(_options){
+        this.__supReset(_options);
+        // TODO
+    };
+    /**
+     * 控件回收
+     * @protected
+     * @method {__destroy}
+     * @return {Void}
+     */
+    _pro.__destroy = function(){
+        this.__supDestroy();
+        // TODO
+    };
+    
+    
+});

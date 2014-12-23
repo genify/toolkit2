c2de132f50f89033e8580c058cce46313a279d89
genify <genify@163.com>
2014-12-23 17:54:54 +0800

    test parser/tag

--- test/parser/tag/test.js
+++ test/parser/tag/test.js
@@ -82,7 +82,7 @@ describe('parser/tag',function(){
         var _doTestFromFile = function(file){
             var ret = {tag:[],text:[],comment:[]};
             new _tag.Tokenizer(
-                _fs.read(process.cwd()+file).join('\n'),{
+                _fs.read(__dirname+'/'+file).join('\n'),{
                     tag:function(event){
                         ret.tag.push(event);
                         //var source = event.source;
@@ -102,13 +102,13 @@ describe('parser/tag',function(){
             return ret;
         };
         // it('should be ok for parsing html file',function(){
-            // _doTestFromFile('/parser/tag/a.html');
+            // _doTestFromFile('a.html');
         // });
         // it('should be ok for parsing freemarker file',function(){
-            // _doTestFromFile('/parser/tag/a.ftl');
+            // _doTestFromFile('a.ftl');
         // });
         it('should be ok for parsing script with tag',function(){
-            var ret = _doTestFromFile('/parser/tag/b.html');
+            var ret = _doTestFromFile('b.html');
             // beg script
             var tag = ret.tag.shift();
             tag.name.should.equal('script');
@@ -118,7 +118,7 @@ describe('parser/tag',function(){
             tag.closed.should.be.true;
         });
         it('should be ok for parsing textarea with content',function(){
-            var ret = _doTestFromFile('/parser/tag/c.html');
+            var ret = _doTestFromFile('c.html');
             // beg textarea
             var tag = ret.tag.shift();
             tag.name.should.equal('textarea');
@@ -129,41 +129,74 @@ describe('parser/tag',function(){
             tag.closed.should.be.true;
         });
         it('should be ok for parsing conditional comments',function(){
-            var ret = _doTestFromFile('/parser/tag/d.html');
+            var ret = _doTestFromFile('d.html');
             ret.comment.length.should.equal(1);
             ret.comment[0].comment.trim().should.equal('Comment content');
         });
     });
     
-    describe('Paser',function(){
-
+    describe('Parser',function(){
         var _doTestFromFile = function(file){
-            var ret = {style:[],script:[],textarea:[],comment:[]};
-            var parser = new _tag.Parser(
-                _fs.read(process.cwd()+file).join('\n'),{
+            var ret = {style:[],script:[],textarea:[],instr:[]};
+            ret.inst = new _tag.Parser(
+                _fs.read(__dirname+'/'+file).join('\n'),{
                     style:function(event){
                         ret.style.push(event);
-                        console.log('STYLE\n%j\n%j',event.config,event.source);
+                        //console.log('STYLE\n%j\n%j',event.config,event.source);
                     },
                     script:function(event){
                         ret.script.push(event);
-                        console.log('SCRIPT\n%j\n%j',event.config,event.source);
+                        //console.log('SCRIPT\n%j\n%j',event.config,event.source);
                     },
                     textarea:function(event){
                         ret.textarea.push(event);
-                        console.log('TEXTAREA\n%j\n%j',event.config,event.source);
+                        //console.log('TEXTAREA\n%j\n%j',event.config,event.source);
                     },
-                    comment:function(event){
-                        ret.comment.push(event);
-                        console.log('COMMENT\n%j\n%s',event.config,event.source);
+                    instruction:function(event){
+                        ret.instr.push(event);
+                        //console.log('INSTRUCTION\n%s\n%j',event.command,event.config);
                     }
                 }
             );
-            ret.inst = parser;
+            return ret;
         };
         it('should be ok for parsing html file',function(){
-            var ret = _doTestFromFile('/parser/tag/a.html');
-            
+            var ret = _doTestFromFile('a.html');
+            // check style
+            ret.style[0].config.href.should.equal('../css/template.css');
+            ret.style[1].config.href.should.equal('../css/app.css');
+            // check script
+            ret.script[0].source.should.not.be.empty;
+            ret.script[1].config.src.should.equal('../javascript/cache/tag.data.js');
+            ret.script[2].config.src.should.equal('../javascript/cache/blog.data.js');
+            ret.script[3].source.should.not.be.empty;
+            ret.script[4].config.src.should.equal('http://nej.netease.com/nej/src/define.js');
+            ret.script[5].source.should.not.be.empty;
+            // check textarea
+            ret.textarea[0].config.should.eql({name:"html","data-src":"module/tab/index.html"});
+            ret.textarea[1].config.should.eql({name:"html","data-src":"module/layout/system/index.html"});
+            ret.textarea[2].config.should.eql({name:"html","data-src":"module/layout/blog/index.html"});
+            ret.textarea[3].config.should.eql({name:"html","data-src":"module/layout/blog.list/index.html"});
+            ret.textarea[4].config.should.eql({name:"html","data-src":"module/layout/setting/index.html"});
+            ret.textarea[5].config.should.eql({name:"html","data-src":"module/layout/setting.account/index.html"});
+            // check instruction
+            ret.instr.should.eql([{"closed":false,"command":"STYLE","config":{"core":false}},{"closed":false,"command":"TEMPLATE"},{"closed":true,"command":"TEMPLATE"},{"closed":false,"command":"MODULE"},{"closed":true,"command":"MODULE"},{"closed":false,"command":"IGNORE"},{"closed":true,"command":"IGNORE"},{"closed":false,"command":"VERSION"},{"closed":false,"command":"DEFINE","config":{"inline":true}}]);
+        });
+        it('should be ok for parsing freemarker file',function(){
+            var ret = _doTestFromFile('a.ftl');
+            // check style
+            ret.style[0].config.href.should.equal('/src/css/page/schedule.css');
+            // check script
+            ret.script[0].source.should.not.be.empty;
+            ret.script[1].config.src.should.equal('${jslib}define.js?${jscnf}');
+            ret.script[2].config.src.should.equal('${jspro}page/schedule/schedule.js');
+            // check textarea
+            ret.textarea[0].config.should.eql({"name":"txt","id":"product-loading"});
+            ret.textarea[0].source.should.not.be.empty;
+            ret.textarea[1].config.should.eql({"name":"jst","id":"product-list"});
+            ret.textarea[1].source.should.not.be.empty;
+            // check instruction
+            ret.instr.should.be.empty;
         });
         
     });

ff361fcc9b11f4dceaf14ea209784149f6b19a60
genify <genify@163.com>
2015-06-01 17:21:34 +0800

    refactor html parser

--- test/parser/html/test.js
+++ test/parser/html/test.js
@@ -1,19 +1,23 @@
-var Parser = require('../../../lib/parser/html.js'),
-    should = require('should');
+var should = require('should'),
+    fs = require('../../../lib/util/file.js'),
+    Parser = require('../../../lib/parser/html.js');
     
 describe('parser/html',function(){
     
     describe('new Parser',function(){
-        // it('should be ok for parse html file',function(){
-            // var parser = new Parser('./parser/html/a.html');
-            // parser.should.be.an.instanceof(Parser);
-        // });
+         it('should be ok for parse html file',function(){
+             var parser = new Parser({
+                 content:fs.read(__dirname+'/a.html').join('\n')
+             });
+             parser.should.be.an.instanceof(Parser);
+             //console.log('%s',JSON.stringify(parser,null,4));
+         });
         it('should be ok for parse freemarker file',function(){
             var parser = new Parser({
-                file:'./parser/html/a.ftl'
+                content:fs.read(__dirname+'/a.ftl').join('\n')
             });
             parser.should.be.an.instanceof(Parser);
-            //console.log('%s',JSON.stringify(parser,null,4));
+            console.log('%s',JSON.stringify(parser,null,4));
         });
     });
     

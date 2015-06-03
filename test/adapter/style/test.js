b92720a0e5cfea058142048822655cf2d265c6aa
genify <genify@163.com>
2015-06-03 13:25:27 +0800

    refactor css adapter

---
+++ test/adapter/style/test.js
@@ -0,0 +1,29 @@
+var should = require('should'),
+    fs  = require('../../../lib/util/file.js'),
+    css = require('../../../lib/adapter/style.js');
+
+describe('adapter/style',function(){
+
+    var parser = new css.Parser({
+        file:'c:/pro/a.css',
+        content:fs.read(__dirname+'/a.css').join('\n')
+    });
+
+    describe('new Parser',function(){
+        it('should instance of Parser',function(){
+            parser.should.be.an.instanceof(css.Parser);
+        });
+    });
+
+    describe('.parse(config)',function(){
+        it('should be ok after parse with config',function(){
+            parser.parse({
+                webRoot:'c:/webapp/'
+            });
+            var ret = parser.stringify().replace(/\s/g,'');
+            var out = fs.read(__dirname+'/r.css').join('\n').replace(/\s/g,'');
+            ret.should.be.eql(out);
+        });
+    });
+
+});

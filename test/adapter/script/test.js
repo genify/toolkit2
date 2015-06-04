7188fe73da120ea4563b315fca38345de0f3d4c9
genify <genify@163.com>
2015-06-04 20:31:08 +0800

    test case for script adapter

--- test/adapter/script/test.js
+++ test/adapter/script/test.js
@@ -19,8 +19,9 @@ describe('adapter/script',function(){
             'a1.js':'(function(x,y){var a=1111;})();if (DEBUG){dosomething(1);}',
             'a2.js':'(function(z){var b=22222;})();',
             'a3.js':fs.read(__dirname+'/../../cases/base/global.js').join('\n'),
-            'b1.js':'var c="ccc";',
+            'b1.js':'var c="ccc";if (CMPT){dosomething2(1);}',
             'b2.js':'var d="ddddd";',
+            'c1.js':'var a;console.log(1);var b;',
             'error.js':'zzzz+function();'
         };
         [
@@ -28,9 +29,9 @@ describe('adapter/script',function(){
                 map:{'a.js':['error.js','a1.js','a2.js','error.js','b1.js','b2.js']},
                 config:{level:0},
                 result:function(ret){
-                    ret.should.have.property('code');
-                    Object.keys(ret.code).should.be.eql(Object.keys(this.map));
-                    console.log('%j',ret);
+                    //ret.should.have.property('code');
+                    //Object.keys(ret.code).should.be.eql(Object.keys(this.map));
+                    //console.log('%j',ret);
                 }
             },
             {
@@ -45,6 +46,15 @@ describe('adapter/script',function(){
             {
                 map:{'a.js':['a1.js','a2.js']},
                 config:{level:3},
+                result:function(ret){
+                    ret.should.have.property('code');
+                    Object.keys(ret.code).should.be.eql(Object.keys(this.map));
+                    console.log('%j',ret);
+                }
+            },
+            {
+                map:{'c.js':['c1.js','b2.js']},
+                config:{dropconsole:true},
                 result:function(ret){
                     ret.should.have.property('code');
                     Object.keys(ret.code).should.be.eql(Object.keys(this.map));

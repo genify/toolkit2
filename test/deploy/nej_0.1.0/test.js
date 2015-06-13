c99a7b843868bda50ef44a580640fae4dd433991
genify <genify@163.com>
2015-06-13 18:19:40 +0800

    logger for all parser

--- test/deploy/nej_0.1.0/test.js
+++ test/deploy/nej_0.1.0/test.js
@@ -6,6 +6,7 @@ describe('deploy',function(){
     describe('new Processor',function(){
 
         it('should be ok for deploy module project with nej 0.1.0+',function(done){
+            this.timeout(20000);
             new Processor({
                 file:__dirname+'/release.conf',
                 done:function(){

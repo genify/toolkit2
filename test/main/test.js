3354c358b93a22a19cdffa5911b63080707513f7
genify <genify@163.com>
2015-07-31 17:09:36 +0800

    support nej export

--- test/main/test.js
+++ test/main/test.js
@@ -7,26 +7,45 @@ describe('main',function(){
 
     describe('.init(output)',function(){
 
-        it('should output release.conf',function(){
-            main.init(__dirname+'/');
-            fs.exist(__dirname+'/release.conf').should.be.true;
+        //it('should output release.conf',function(){
+        //    main.init(__dirname+'/');
+        //    fs.exist(__dirname+'/release.conf').should.be.true;
+        //});
+
+    });
+
+    describe('.export(list,config,callback)',function(){
+
+        //it('should be ok for export nev',function(done){
+        //    this.timeout(60000);
+        //    var root = path.absolute(
+        //        '../cases/nev/',
+        //        __dirname+'/'
+        //    );
+        //    main.export(['define.js?pro='+root+'src/',root+'build/nev.js'],{
+        //        output:root+'build/nev.min.js',
+        //        codeWrap:'(function(I$){%s})();'
+        //    },function(){
+        //        fs.exist(root+'build/nev.min.js').should.be.true;
+        //        done();
+        //    });
+        //});
+
+        it('should be ok for export nej module',function(done){
+            this.timeout(60000);
+            var output = path.absolute(
+                './xdr.min.js',
+                __dirname+'/'
+            );
+            main.export(['http://fed.hz.netease.com/git/nej2/src/define.js','util/ajax/xdr'],{
+                output:output
+            },function(){
+                fs.exist(output).should.be.true;
+                done();
+            });
         });
 
     });
 
-    describe('.export(list,config,callback)',function(done){
-        this.timeout(60000);
-        var root = path.absolute(
-            '../cases/nev/',
-            __dirname+'/'
-        );
-        main.export(['define.js?pro='+root+'src/',root+'build/nev.js'],{
-            output:root+'build/nev.min.js',
-            codeWrap:'(function(I$){%s})();'
-        },function(){
-            fs.exist(root+'build/nev.min.js').should.be.true;
-            done();
-        });
-    });
 
 });

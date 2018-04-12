eddd030a57e78ad973a1ed04304927ecf70fef67
genify <genify@gmail.com>
2018-04-12 23:33:35 +0800

    support dir copy for res plugin

--- test/cases/merge/webapp/lib/component/a.js
+++ test/cases/merge/webapp/lib/component/a.js
@@ -1,7 +1,11 @@
 NEJ.define([
     'css!./a.css',
-    'res!./res/c.png'
-],function (x, u) {
+    'res!./res/img/',
+    'res!./res/c.png',
+    'res!./res/img/b.png'
+],function (x, u, y, z) {
     console.log(x);
     console.log('inject uri -> '+u);
+    console.log('inject dir -> '+y);
+    console.log('inject img -> '+z);
 });

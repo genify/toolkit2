51d67ce3cea8cc072cc135ad2f2f1380d5a5fbfe
genify <genify@gmail.com>
2019-02-12 14:23:20 +0800

    change module insert point from before </body> to before script point

--- test/parser/html/test.js
+++ test/parser/html/test.js
@@ -5,25 +5,26 @@ var should = require('should'),
 describe('parser/html',function(){
     
     describe('new Parser',function(){
-         it('should be ok for parse html file',function(){
-             var parser = new Parser({
-                 file:__dirname+'/create.ftl',
-                 noParseFlag:3,
-                 content:fs.read(__dirname+'/create.ftl').join('\n')
-             });
-             console.log('%s',JSON.stringify(parser,null,4));
-             parser.scan(function(event){
-                 console.log('%s : %s',event.type,event.content);
-             });
-             parser.should.be.an.instanceof(Parser);
-         });
-        //it('should be ok for parse freemarker file',function(){
-        //    var parser = new Parser({
-        //        content:fs.read(__dirname+'/a.ftl').join('\n')
-        //    });
-        //    parser.should.be.an.instanceof(Parser);
-        //    //console.log('%s',JSON.stringify(parser,null,4));
-        //});
+         // it('should be ok for parse html file',function(){
+         //     var parser = new Parser({
+         //         file:__dirname+'/create.ftl',
+         //         noParseFlag:3,
+         //         content:fs.read(__dirname+'/create.ftl').join('\n')
+         //     });
+         //     console.log('%s',JSON.stringify(parser,null,4));
+         //     parser.scan(function(event){
+         //         console.log('%s : %s',event.type,event.content);
+         //     });
+         //     parser.should.be.an.instanceof(Parser);
+         // });
+        it('should be ok for parse freemarker file',function(){
+           var parser = new Parser({
+               file:__dirname+'/a.ftl',
+               content:fs.read(__dirname+'/a.ftl').join('\n')
+           });
+           parser.should.be.an.instanceof(Parser);
+           console.log('%s',JSON.stringify(parser,null,4));
+        });
     });
     
 });
